340de2046680a8e3870dc0a5ad2b65591c75b5e0
zolfaShefreie <z.shefreie@gmail.com>
2021-06-27 22:18:26 +0430

    add needed_train func to model process

--- model_process.py
+++ model_process.py
@@ -9,14 +9,25 @@ class ModelProcess:
                                             max_depth=17, bootstrap=False)
         self.file_path = file_path
 
+    def needed_train(self):
+        try:
+            open(self.file_path, 'r')
+        except FileNotFoundError:
+            return True
+        return False
+
     def train(self, x_train, y_train):
         self.model.fit(x_train, y_train)
+        self.save_model()
 
     def save_model(self):
         joblib.dump(self.model, open(self.file_path, 'wb'))
 
     def load_model(self):
-        joblib.load(open(self.file_path, 'rb'))
+        self.model = joblib.load(open(self.file_path, 'rb'))
 
     def get_result_test(self, x_test):
         return self.model.predict(x_test)
+
+    def print_result_validation(self):
+        pass
